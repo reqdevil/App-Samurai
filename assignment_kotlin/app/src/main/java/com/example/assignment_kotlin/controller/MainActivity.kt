f551d622793c9a30e47471639248337a0c55df13
Berk Er <berkerr.tr@gmail.com>
2022-04-04 16:06:57 +0300

    Code Cleanup
    The codes in the previous check-in were cleaned.

--- assignment_kotlin/app/src/main/java/com/example/assignment_kotlin/controller/MainActivity.kt
+++ assignment_kotlin/app/src/main/java/com/example/assignment_kotlin/controller/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.assignment_kotlin.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.LayoutDirection
 import android.util.Log
 import android.view.View
 import android.widget.ListView
@@ -10,45 +9,37 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.assignment_kotlin.R
 import com.example.assignment_kotlin.model.Story
+import com.example.assignment_kotlin.service.DataService
 import com.example.assignment_kotlin.utilities.personList
-import com.example.assignment_kotlin.view.StoryListAdapter
-import com.example.assignment_kotlin.view.StoryRecyclerAdapter
+import com.example.assignment_kotlin.view.StoryAdapter
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String = "Main Activity"
 
-    private lateinit var storyRecylerView: RecyclerView
-    private lateinit var storyListView: ListView
+    private lateinit var storyView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        storyRecylerView = findViewById<RecyclerView>(R.id.storyRecyclerView)
-        storyListView = findViewById(R.id.storyListView)
+        storyView = findViewById(R.id.storyView)
 
         loadStoryRecyclerView()
-        loadStoryListView()
     }
 
     private fun loadStoryRecyclerView() {
-        storyRecylerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        storyView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
         val data = ArrayList<Story>()
         for (person in personList) {
             data.add(Story(person.image, person.name))
         }
 
-        val adapter = StoryRecyclerAdapter(data)
-        storyRecylerView.adapter = adapter
+        val adapter = StoryAdapter(data)
+        storyView.adapter = adapter
     }
 
-    private fun loadStoryListView() {
-        val mLayoutManager = LinearLayoutManager(applicationContext)
-        mLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
-
-        storyListView.layoutDirection = View.LAYOUT_DIRECTION_LTR
-        Log.e(TAG, storyListView.layoutDirection.toString())
-        storyListView.adapter = StoryListAdapter(this, personList)
+    fun downloadPicture(view: View) {
+        DataService.instance.downloadJPEG("image")
     }
 }
--- assignment_kotlin/app/src/main/java/com/example/assignment_kotlin/view/StoryAdapter.kt
+++ assignment_kotlin/app/src/main/java/com/example/assignment_kotlin/view/StoryAdapter.kt
@@ -1,15 +1,21 @@
 package com.example.assignment_kotlin.view
 
+import android.annotation.SuppressLint
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.assignment_kotlin.R
 import com.example.assignment_kotlin.model.Story
 
-class StoryRecyclerAdapter(private val list: List<Story>) : RecyclerView.Adapter<StoryRecyclerAdapter.ViewHolder>() {
+private val TAG = "Story Adapter"
+
+class StoryAdapter(private val list: List<Story>) : RecyclerView.Adapter<StoryAdapter.ViewHolder>() {
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val imageView: ImageView = itemView.findViewById(R.id.imageView)
         val textView: TextView = itemView.findViewById(R.id.textView)
@@ -27,6 +33,10 @@ class StoryRecyclerAdapter(private val list: List<Story>) : RecyclerView.Adapter
 
         holder.imageView.setImageResource(story.image)
         holder.textView.text = story.text
+
+        holder.imageView.setOnClickListener {
+            Log.e(TAG, "Click Called")
+        }
     }
 
     override fun getItemCount(): Int {
